c61b19e7fb56a019945d28457cf1a8114cb9dafb
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-02-22 10:04:37 +0100

    Added .API removed .app .data

---
+++ BlazorTickets.Api/Models/TagModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTickets_Grupp.Data.DataBase.Models;
+
+public class TagModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } // Exempel: "CSharp", "JavaScript"
+    public List<TicketTag> TicketTags { get; set; } = new List<TicketTag>();
+}
+
